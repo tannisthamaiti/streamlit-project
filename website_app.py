acae2616f4b9dc5be0d167cdffb563cf812af05a
tannisthamaiti <tannistha.iitkgp@gmail.com>
2023-09-29 14:44:48 -0400

    website

--- website_app.py
+++ website_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
+#from streamlit_option_menu import option_menu
 #import streamlit.components.v1 as html
 from  PIL import Image
 import numpy as np
 import pandas as pd
 #import streamlit.components.v1 as components
 import base64
-from streamlit_text_rating.st_text_rater import st_text_rater
+#from streamlit_text_rating.st_text_rater import st_text_rater
 
 
 
